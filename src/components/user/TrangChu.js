fef748f8260d9123fd2de1344af3514cf9cca154
Buu Nhut <nhut.nta@gmail.com>
2024-01-23 14:16:11 +0700

    hidden menu

--- src/components/user/TrangChu.js
+++ src/components/user/TrangChu.js
@@ -1,21 +1,59 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 
 const TrangChu = () => {
+    const [show, setShow] = useState(true);
+    const [prevScrollY, setPrevScrollY] = useState(0);
+
+    const handleScroll = () => {
+        const currentScrollY = window.scrollY || window.pageYOffset;
+
+        // console.log(currentScrollY)
+
+        if (currentScrollY >= prevScrollY) {
+            setShow(false);
+        } else {
+            setShow(true);
+        }
+        if (currentScrollY === 0) {
+            setShow(true);
+        }
+        setPrevScrollY(currentScrollY);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [prevScrollY]);
+
+    // console.log('Previous Scroll Y:', prevScrollY);
     return (
         <div id="trangChu">
-            <header>
-                <Header />
-            </header>
+            {
+                show ? (
+                    <header>
+                        <Header />
+                    </header>
+                ) : (null)
+            }
+
             <main>
                 <Outlet />
             </main>
-            <footer>
-                <Footer />
-            </footer>
-            
+            {
+                show ? (
+                    <footer>
+                        <Footer />
+                    </footer>
+
+                ) : (null)
+            }
+
         </div>
     );
 };
